24e9883eec239db30dc60d25a75bc94a8a0e1c51
codeWithArif1982 <codewitharif1982@gmail.com>
2023-08-05 11:35:54 -0400

    Redux TODO Project

---
+++ Todo-redux/src/components/todo/ToDoItem.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import Swal from "sweetalert2";
+import { deleteItem } from "../../redux/features/todo/toDoSlice";
+import styles from "./ToDoItem.module.css";
+import { updateItem } from "./alert/editAlert";
+
+const ToDoItem = () => {
+  const todoList = useSelector((state) => state.toDo.todoList);
+  const dispatch = useDispatch();
+
+  const [checkedItems, setCheckedItems] = useState({});
+
+  const handleCheckboxChange = (itemId) => {
+    setCheckedItems((prevCheckedItems) => ({
+      ...prevCheckedItems,
+      [itemId]: !prevCheckedItems[itemId],
+    }));
+  };
+
+  const handleItemDelete = (itemId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(deleteItem(itemId));
+        Swal.fire("Deleted!", "Your task has been deleted.", "success");
+      }
+    });
+
+    setCheckedItems((prevCheckedItems) => {
+      const { [itemId]: removedItem, ...rest } = prevCheckedItems;
+      return rest;
+    });
+  };
+
+  return (
+    <>
+      {todoList.length !== 0 &&
+        todoList.map((itemObj) => {
+          const { id, item } = itemObj;
+          const isItemChecked = checkedItems[id];
+
+          return (
+            <div
+              key={id}
+              className={`card-body my-3 shadow-sm ${styles.cardDiv}`}
+            >
+              <input
+                className={`${styles.checkBox}`}
+                type="checkbox"
+                checked={isItemChecked}
+                onChange={() => handleCheckboxChange(id)}
+              />
+              <h5
+                style={{
+                  textDecorationColor: isItemChecked,
+                  textDecoration: isItemChecked
+                    ? "line-through #e74d42"
+                    : "none",
+                }}
+              >
+                {item}
+              </h5>
+              <button
+                style={{ display: isItemChecked && "none" }}
+                className={`me-3 ${styles.editbtn}`}
+                onClick={() => {
+                  updateItem(id, itemObj);
+                }}
+              >
+                Edit
+                <AiOutlineEdit className={styles.editIcon} />
+              </button>
+              {isItemChecked && (
+                <p
+                  style={{
+                    marginRight: "8px",
+                    marginTop: "14px",
+                    color: "#acaaad",
+                  }}
+                >
+                  completed
+                </p>
+              )}
+              <button
+                onClick={() => {
+                  handleItemDelete(id);
+                }}
+                className={`${styles.deletebtn}`}
+              >
+                Delete
+                <AiOutlineDelete className={styles.deleteIcon} />
+              </button>
+            </div>
+          );
+        })}
+    </>
+  );
+};
+
+export default ToDoItem;
